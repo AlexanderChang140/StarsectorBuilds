700a7d0a769074850cedc2692b87691f8ba678b6
Alex <alexanderchang140@gmail.com>
2025-11-27 00:00:58 -0800

    fix: change built_in_wings table to use ship_id

--- backend/src/db/sql/schema/008_create_built_in.sql
+++ backend/src/db/sql/schema/008_create_built_in.sql
@@ -20,9 +20,9 @@ CREATE TABLE IF NOT EXISTS built_in_hullmods
 CREATE TABLE IF NOT EXISTS built_in_wings
 (
     ship_instance_id integer NOT NULL,
-    wing_id integer NOT NULL,
+    ship_id integer NOT NULL,
     num_wings integer NOT NULL,
-    PRIMARY KEY (ship_instance_id, wing_id),
+    PRIMARY KEY (ship_instance_id, ship_id),
     FOREIGN KEY (ship_instance_id) REFERENCES ship_instances(id) ON DELETE CASCADE,
-    FOREIGN KEY (wing_id) REFERENCES wings(id)
+    FOREIGN KEY (ship_id) REFERENCES ships(id)
 );
