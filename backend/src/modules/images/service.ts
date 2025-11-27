1d843104382e01673b3d37a41f60c2995afdef4d
Alex <alexanderchang140@gmail.com>
2025-11-27 01:31:39 -0800

    fix: remove invalid function

--- backend/src/modules/images/service.ts
+++ backend/src/modules/images/service.ts
@@ -1,10 +1,5 @@
 import type { Image, InsertedImage } from './types.ts';
 import { makeInsertReturn } from '../../db/helpers/insert.ts';
-import { makeSelect } from '../../db/helpers/select.ts';
-
-export const getImageFromId = makeSelect<Image, Pick<Image, 'id'>>('images', [
-    'file_path',
-]);
 
 export const insertImage = makeInsertReturn<InsertedImage, Image>('images', [
     'file_hash',
