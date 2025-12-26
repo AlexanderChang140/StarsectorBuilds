9995bb8a8aa37f6c7a71c1867e76711f8084e083
Alex <alexanderchang140@gmail.com>
2025-12-25 20:14:08 -0800

    refactor: add styles alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,6 +13,14 @@ export default defineConfig(({ mode }) => {
             proxy: {
                 '/api': env.VITE_API_URL,
             },
+        },
+        resolve: {
+            alias: [
+                {
+                    find: '@styles',
+                    replacement: path.resolve(__dirname, 'src', 'styles'),
+                },
+            ],
         },
     };
 });
