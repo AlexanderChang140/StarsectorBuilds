c0dd12d92fd07e0bf8feebf35018f410f78caba9
CAT <alexanderchang140@gmail.com>
2025-12-30 23:38:13 -0800

    feat: add routes to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Layout from './components/layout/Layout.tsx';
 import { AuthProvider } from './modules/auth/AuthProvider.tsx';
 import Auth from './modules/auth/pages/Auth.tsx';
+import NewBuild from './modules/builds/pages/NewBuild.tsx';
 import HullmodTable from './modules/hullmods/pages/HullmodTable.tsx';
+import ShipDisplay from './modules/ships/pages/ShipDisplay.tsx';
 import ShipTable from './modules/ships/pages/ShipTable.tsx';
 import WeaponDisplay from './modules/weapons/pages/WeaponDisplay.tsx';
 import WeaponTable from './modules/weapons/pages/WeaponTable.tsx';
@@ -20,15 +22,31 @@ function App() {
                     <Route element={<Layout />}>
                         <Route path="/" element={<Home />} />
                         <Route path="/auth" element={<Auth />} />
+
                         <Route path="/weapons" element={<WeaponTable />} />
                         <Route
-                            path="/weapons/:id"
+                            path="/weapons/:weaponId"
                             element={<WeaponDisplay />}
                         />
+                        <Route
+                            path="/weapons/:weaponId/version/:versionId"
+                            element={<WeaponDisplay />}
+                        />
+
                         <Route path="/hullmods" element={<HullmodTable />} />
                         <Route path="/hullmods/:id" element={TODO} />
 
                         <Route path="/ships" element={<ShipTable />} />
+                        <Route
+                            path="/ships/:shipId"
+                            element={<ShipDisplay />}
+                        />
+                        <Route
+                            path="/ships/:shipId/:versionId"
+                            element={<ShipDisplay />}
+                        />
+
+                        <Route path="/builds/new/:id" element={<NewBuild />} />
 
                         <Route path="/user/:id" element={TODO} />
                         <Route path="/settings" element={TODO} />
