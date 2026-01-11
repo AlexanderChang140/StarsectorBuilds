60bb7f663c051b2eb12c99d812a275324ad524bc
CAT <alexanderchang140@gmail.com>
2026-01-11 02:54:48 -0800

    feat: add sanitizeAll function

--- backend/src/utils/sanitize.ts
+++ backend/src/utils/sanitize.ts
@@ -2,11 +2,12 @@ import { filterKeys, filterValues } from './helpers.ts';
 import { clamp } from './math.ts';
 import type { Filter } from '../db/helpers/filter.ts';
 import type { Order } from '../db/types.ts';
+import type { Options } from '../types/generic.ts';
 
 export function sanitizeFilter<T>(
     filter: Filter<T> | undefined,
     keys: readonly (keyof T)[],
-): Partial<T> {
+): Filter<T> {
     const filtered = filterKeys(filter ?? {}, keys);
     const sanitized = Object.fromEntries(
         Object.entries(filtered).filter(
@@ -15,7 +16,7 @@ export function sanitizeFilter<T>(
                 v !== null &&
                 (!Array.isArray(v) || v.length !== 0),
         ),
-    ) as Partial<T>;
+    ) as Filter<T>;
 
     return sanitized;
 }
@@ -35,4 +36,18 @@ export function sanitizeLimit(
 
 export function sanitizeOffset(offset: number | undefined): number {
     return Math.max(offset ?? 0, 0);
+}
+
+export function sanitizeAll<T, TKeys extends readonly (keyof T)[]>(
+    options: Options<T>,
+    filterColumns: TKeys,
+    defaultLimit = 20,
+): Options<T> {
+    return {
+        filter: sanitizeFilter(options.filter, filterColumns),
+        order: sanitizeOrder(options.order),
+        limit: sanitizeLimit(options.limit, defaultLimit),
+        offset: sanitizeOffset(options.offset),
+        client: options.client,
+    };
 }
