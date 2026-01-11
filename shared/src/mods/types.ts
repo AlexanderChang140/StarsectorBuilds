858a89267c183cb0ea79b6aa8b500dfa4582f09c
CAT <alexanderchang140@gmail.com>
2026-01-11 03:28:44 -0800

    feat: add shared type ModVersionDTO

---
+++ shared/src/mods/types.ts
@@ -0,0 +1,10 @@
+export type ModVersionDTO = {
+    data_changed: boolean;
+    major: number;
+    minor: number;
+    mod_code: string;
+    mod_id: number;
+    mod_name: string | null;
+    mod_version_id: number;
+    patch: string;
+};
