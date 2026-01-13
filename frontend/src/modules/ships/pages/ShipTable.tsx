04f99c14126d3ca193809c097418cd5f8f8965f3
CAT <alexanderchang140@gmail.com>
2026-01-12 22:19:51 -0800

    feat: enhance ShipTable with filtering capabilities and mod version management

--- frontend/src/modules/ships/pages/ShipTable.tsx
+++ frontend/src/modules/ships/pages/ShipTable.tsx
@@ -1,36 +1,84 @@
+import type { ModDTO, ModVersionDTO } from '@shared/mods/types';
 import { useSearchParams } from 'react-router';
 
 import { DataTable } from '@/components/DataTable/DataTable';
+import DataTableFilterChips from '@/components/DataTable/DataTableFilterChips';
 import DataTableFilters, {
     type TableFilter,
 } from '@/components/DataTable/DataTableFilters';
 import { SHIP_TABLE_ROW_KEYS, type ShipTableRows } from '@/modules/ships/types';
-import { DataTable } from '@/pages/DataTable/DataTable';
 import { humanizeKeys } from '@/utils/humanizeKeys';
+import toVersionString from '@/utils/toVersionString';
 
 export default function ShipTable() {
-    return DataTable<
-        ShipTableRows,
-        typeof SHIP_TABLE_ROW_KEYS,
-        typeof KEY_ORDER
-    >({
-        dataConfig: {
-            endpoint: '/api/ship-versions',
-            queryKey: 'shipsTable',
-            initialSort: { sortField: 'display_name', sortOrder: 'ASC' },
-        },
-        tableConfig: {
-            displayMap,
-            tableKeys: SHIP_TABLE_ROW_KEYS,
-            keyOrder: KEY_ORDER,
-        },
-        link: {
-            linkField: 'display_name',
-            linkFn: (row) =>
-                `/ships/${Number(row.ship_id)}/${Number(row.ship_version_id)}`,
-        },
-        title: 'Ships',
-    });
+    const [searchParams, setSearchParams] = useSearchParams();
+    const filters = (
+        <DataTableFilters
+            key={1}
+            tableFilters={FILTERS}
+            setSearchParams={setSearchParams}
+        />
+    );
+
+    const filterChips = (
+        <DataTableFilterChips<
+            ModDTO,
+            typeof MOD_KEYS,
+            ModVersionDTO,
+            typeof MOD_VERSION_KEYS
+        >
+            key={0}
+            category={{
+                categoryQueryKey: 'shipTableMods',
+                categoryEndpoint: '/api/mods/search/display_name',
+                categoryKeys: MOD_KEYS,
+                categoryValueTransform: (mod) => String(mod.id),
+                categoryLabelTransform: (mod) => String(mod.display_name),
+            }}
+            chips={{
+                chipQueryKey: 'shipTableModVersions',
+                chipEndpoint: '/api/mod-versions',
+                chipKeys: MOD_VERSION_KEYS,
+                chipFilterKey: 'mod_version_id',
+                chipCategoryKey: 'mod_id',
+                chipValueTransform: (v) => String(v.mod_version_id),
+                chipLabelTransform: (v) =>
+                    toVersionString(v.major, v.minor, v.patch),
+                chipCategoryLabelTransform: (v) => v.mod_name ?? '',
+                chipCategoryValueTransform: (v) => String(v.mod_id),
+            }}
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+        />
+    );
+
+    return (
+        <DataTable<ShipTableRows, typeof SHIP_TABLE_ROW_KEYS, typeof KEY_ORDER>
+            dataConfig={{
+                endpoint: '/api/ship-versions',
+                queryKey: 'shipsTable',
+                initialSort: { sortField: 'display_name', sortOrder: 'ASC' },
+            }}
+            tableConfig={{
+                displayMap,
+                tableKeys: SHIP_TABLE_ROW_KEYS,
+                keyOrder: KEY_ORDER,
+            }}
+            searchConfig={{
+                externalSearchParams: searchParams,
+                setExternalSearchParams: setSearchParams,
+            }}
+            filters={[filterChips, filters]}
+            link={{
+                linkField: 'display_name',
+                linkFn: (row) =>
+                    `/ships/${Number(row.ship_id)}/${Number(
+                        row.ship_version_id,
+                    )}`,
+            }}
+            title="Ships"
+        ></DataTable>
+    );
 }
 
 const KEY_ORDER = [
@@ -75,3 +123,42 @@ const KEY_ORDER = [
 ] as const satisfies readonly (keyof ShipTableRows)[];
 
 const displayMap = humanizeKeys(KEY_ORDER);
+
+const FILTERS = [
+    {
+        field: 'ship_size',
+        label: 'Ship Size',
+        items: [
+            { value: 'FRIGATE', label: 'FRIGATE' },
+            { value: 'DESTROYER', label: 'DESTROYER' },
+            { value: 'CRUISER', label: 'CRUISER' },
+            { value: 'CAPITAL_SHIP', label: 'CAPITAL' },
+        ],
+        isMultiSelect: true,
+    },
+    {
+        field: 'shield_type',
+        label: 'Shield Type',
+        items: [
+            { value: 'NONE', label: 'NONE' },
+            { value: 'OMNI', label: 'OMNI' },
+            { value: 'FULL', label: 'FULL' },
+            { value: 'PHASE', label: 'PHASE' },
+        ],
+        isMultiSelect: true,
+    },
+] as const satisfies readonly TableFilter[];
+
+const MOD_KEYS = [
+    'id',
+    'display_name',
+] as const satisfies readonly (keyof ModDTO)[];
+
+const MOD_VERSION_KEYS = [
+    'mod_id',
+    'mod_name',
+    'mod_version_id',
+    'major',
+    'minor',
+    'patch',
+] as const satisfies readonly (keyof ModVersionDTO)[];
